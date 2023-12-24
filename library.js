5fc536737701fd3e2cb674f2b876783ec01e6276
Travis Hawk <hawk.tr@gmail.com>
2023-12-23 17:40:23 -0800

    add framework of functions and added some test data

--- library.js
+++ library.js
@@ -1,4 +1,11 @@
-const myLibrary = [];
+const myLibrary = [
+    {title:"The Hobbit",author:"J.R.R Tolkien",pages:293,read:"not yet read"},
+    {title:"The Silmarillion",author:"J.R.R Tolkien",pages:365,read:"not yet read"},
+    {title:"Angels and Demons",author:"Dan Brown",pages:448,read:"read"}
+];
+
+const body = document.querySelector("body");
+const bookListContainer = document.querySelector("#bookList");
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -12,7 +19,24 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
+
     
+}
+
+function displayBooks(){
+    bookListContainer.append()
+
+    for (var book of myLibrary){
+        
+        
+        
+        console.log(`
+        Title:${book.title}\n
+        Author:${book.author}\n
+        Page Count:${book.pages}\n
+        Read Status:${book.read}\n
+        `)
+    }
 }
 
 // const book1 = new Book("The Hobbit","J.R.R Tolkien",293,"not yet read");
